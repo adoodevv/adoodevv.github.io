a7b0e2e846a1a3cc615b423701bc3d77d28ee635
Adoo Jonathan <adoojonathan412@gmail.com>
2023-08-19 11:57:57 -0400

    changes in hbover behaviour

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const links =
 document.querySelectorAll('.link');
 links.forEach(link => {
 link.addEventListener('mouseover', () => {
-    link.style.backgroundColor = '#f0f0f0';
+    link.style.backgroundColor = '#3498db';
 });
 link.addEventListener('mouseout', () => {
     link.style.backgroundColor = '#3498db';
