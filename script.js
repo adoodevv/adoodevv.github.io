8baf8ce6b4c286e28f35210ee951f58731884c15
Adoo Jonathan <adoojonathan412@gmail.com>
2023-08-19 11:55:40 -0400

    updates

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const links =
 document.querySelectorAll('.link');
 links.forEach(link => {
 link.addEventListener('mouseover', () => {
-    link.style.backgroundColor = '#e74c3c';
+    link.style.backgroundColor = '#f0f0f0';
 });
 link.addEventListener('mouseout', () => {
     link.style.backgroundColor = '#3498db';
