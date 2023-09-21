866aee05821f10240c08c2d6744ee70df6d88bdb
[Jonathan Adoo] <[adoojonathan412@gmail.com]>
2023-09-20 20:14:38 -0700

    font and minor edits

--- script.js
+++ script.js
@@ -5,6 +5,6 @@ link.addEventListener('mouseover', () => {
     link.style.backgroundColor = '#3498db';
 });
 link.addEventListener('mouseout', () => {
-    link.style.backgroundColor = '#3498db';
+    link.style.backgroundColor = 'rgb(52, 152, 219, 0.7)';
 });
 });
